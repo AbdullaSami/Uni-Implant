f1fa6380020fa143eadd7c98d18ff9c26e4f2b76
abood <>
2025-10-16 15:50:15 +0300

    fix footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -60,44 +60,13 @@
 
             <div class="flex items-center gap-3 mb-3 text-gray-800">
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" class="h-5 w-5"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path fill="#c70a0c" d="M112 128C85.5 128 64 149.5 64 176C64 191.1 71.1 205.3 83.2 214.4L291.2 370.4C308.3 383.2 331.7 383.2 348.8 370.4L556.8 214.4C568.9 205.3 576 191.1 576 176C576 149.5 554.5 128 528 128L112 128zM64 260L64 448C64 483.3 92.7 512 128 512L512 512C547.3 512 576 483.3 576 448L576 260L377.6 408.8C343.5 434.4 296.5 434.4 262.4 408.8L64 260z"/></svg>
-              <span>{{$t('footer.contact.email')}}</span>
+              <span>info@janat-abasaten.com</span>
             </div>
 
             <div class="flex items-center gap-3 mb-5 text-gray-800">
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" class="h-5 w-5"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path fill="#c70a0c" d="M224.2 89C216.3 70.1 195.7 60.1 176.1 65.4L170.6 66.9C106 84.5 50.8 147.1 66.9 223.3C104 398.3 241.7 536 416.7 573.1C493 589.3 555.5 534 573.1 469.4L574.6 463.9C580 444.2 569.9 423.6 551.1 415.8L453.8 375.3C437.3 368.4 418.2 373.2 406.8 387.1L368.2 434.3C297.9 399.4 241.3 341 208.8 269.3L253 233.3C266.9 222 271.6 202.9 264.8 186.3L224.2 89z"/></svg>
               <span>{{$t('footer.contact.phone')}}</span>
             </div>
-
-            <!-- Social Icons -->
-            <!-- <div class="flex space-x-4"> -->
-              <!-- X (Twitter) -->
-              <!-- <a href="#" class="w-9 h-9 flex items-center justify-center rounded-full bg-black text-white hover:bg-red-600 transition">
-                <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 512 512">
-                  <path fill="currentColor" d="M512 97.2..."/>
-                </svg>
-              </a> -->
-
-              <!-- Facebook -->
-              <!-- <a href="#" class="w-9 h-9 flex items-center justify-center rounded-full bg-black text-white hover:bg-red-600 transition">
-                <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 320 512">
-                  <path fill="currentColor" d="M279.14 288l14.22-92.66h-88.91v-60.13..."/>
-                </svg>
-              </a> -->
-
-              <!-- LinkedIn -->
-              <!-- <a href="#" class="w-9 h-9 flex items-center justify-center rounded-full bg-black text-white hover:bg-red-600 transition">
-                <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 448 512">
-                  <path fill="currentColor" d="M100.28 448H7.4V148.9h92.88zM53.79..."/>
-                </svg>
-              </a> -->
-
-              <!-- Instagram -->
-              <!-- <a href="#" class="w-9 h-9 flex items-center justify-center rounded-full bg-black text-white hover:bg-red-600 transition">
-                <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 448 512">
-                  <path fill="currentColor" d="M224.1 141c-63.6 0-114.9 51.3..."/>
-                </svg>
-              </a> -->
-            <!-- </div> -->
           </div>
         </div>
       </div>
